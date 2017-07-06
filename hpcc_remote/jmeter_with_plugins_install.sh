a2fbe90018f7db5e9e8de6ea8f07440444bf7009
dungvt.dev <vutrungdungbk@gmail.com>
2017-07-07 06:39:31 +0700

    update jmeter install script

--- hpcc_remote/jmeter_with_plugins_install.sh
+++ hpcc_remote/jmeter_with_plugins_install.sh
@@ -5,25 +5,30 @@
 
 JMETER=jmeter3.2
 JMETER_HOME=$pwd/jmeter3.2
+CURRENT_HOME=$pwd
 # install jre
 apt update
+
+echo "******************************** Install default jre **********************************"
 apt install default-jre
 
 # download apache-jmeter
+echo "******************************** Download apache jmeter ********************************"
 wget http://mirrors.viethosting.com/apache//jmeter/binaries/apache-jmeter-3.2.tgz
 tar -zxvf apache-jmeter-3.2.tgz 
 mv apache-jmeter-3.2 $JMETER
 cd $JMETER/
 
 # download plugins jar
+echo "******************************** Download plugins manager *******************************"
 wget https://repo1.maven.org/maven2/kg/apc/jmeter-plugins-manager/0.13/jmeter-plugins-manager-0.13.jar
-mv jmeter-plugins-manager-0.13.jar $JMETER_HOME/lib/ext/plugins-manager.jar
+mv jmeter-plugins-manager-0.13.jar ./lib/ext/plugins-manager.jar
 
 CMDRUNNER=cmdrunner-2.0.jar
 if ! [ -f "$CMDRUNNER" ];
 then
     wget https://repo1.maven.org/maven2/kg/apc/cmdrunner/2.0/cmdrunner-2.0.jar
-    mv cmdrunner-2.0.jar $JMETER_HOME/lib/cmdrunner-2.0.jar
+    mv cmdrunner-2.0.jar ./lib/cmdrunner-2.0.jar
 fi
 
 PLUGINCMD=PluginsManagerCMD.sh
@@ -33,5 +38,7 @@ then
 fi
 
 # install plugins
+echo "******************************** Install jmeter plugins ***********************************"
+echo "********************** Install Ultimate Thread Group plugins ******************************"
 ./bin/PluginsManagerCMD.sh install jpgc-casutg
 
