b7a804ff5c320a061d8896f74cc7f49e894aede8
dungvt.dev <vutrungdungbk@gmail.com>
2017-07-09 16:34:10 +0700

    update exceptions

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -1,10 +1,23 @@
-class InvalidActionException(Exception):
-    pass
+class BaseException(Exception):
+    __ex_name__ = 'BaseException'
+
+    def __init__(self, *args, **kwargs, copy=None):
+        Exception.__init__(self, *args, **kwargs)
+        if copy AND isinstance(copy, Exception):
+            self.message = 'From: %s, %s' % (copy.__ex_name__, copy.message)
 
 
-class ExistsException(Exception):
-    pass
+class ActionInvalidException(BaseException):
+    __ex_name__ = 'ActionInvalidException'
 
 
-class NotEnoughParams(Exception):
-    pass
+class ActionErrorException(BaseException):
+    __ex_name__ = 'ActionErrorException'
+
+
+class ExistsException(BaseException):
+    __ex_name__ = 'ExistsException'
+
+
+class NotEnoughParams(BaseException):
+    __ex_name__ = 'NotEnoughParams'
