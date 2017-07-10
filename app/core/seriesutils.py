7efa83510e5f0aaedc6d456fb26db8ea8fa02275
dungvt.dev <vutrungdungbk@gmail.com>
2017-07-10 15:16:23 +0700

    add series utils

---
+++ app/core/seriesutils.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+
+def timevaluepair_to_pdseries(timevalues):
+    df = timevalues if isinstance(timevalues, pd.DataFrame) \
+        else pd.DataFrame(timevalues)
+    convert = 1000000000
+    df = df.set_index(pd.to_datetime(df[0] * convert))[1]
+    return df
+
+
+def resample(pdtimeseries, sampleMinute):
+    sample = '%sT' % sampleMinute
+    df = pdtimeseries.resample(sample).mean()
+    return df
+
+
+def get_newestseries(pdtimeseries, max_fault_point=0):
+    # check series khong bi gian doan
+    count = 0
+    from_idx = 0
+    for i in range(len(pdtimeseries))[::-1]:
+        if np.isnan(pdtimeseries[i]):
+            count = count + 1
+        else:
+            from_idx = i
+            count = 0
+        if count > max_fault_point:
+            # lay doan tu from_idx
+            s = pdtimeseries[from_idx:]
+            # interpolate
+            s = s.interpolate()
+            return s
+    return pdtimeseries.interpolate()
--- app/tests/manual_tests/plugins/test_influxdb_series_read.py
+++ app/tests/manual_tests/plugins/test_influxdb_series_read.py
@@ -3,7 +3,8 @@ from plugins.influxdb import InfluxdbSeriesReadPlugin
 
 config = {
     'endpoint': '192.168.122.124',
-    'db': 'test_db',
+    # 'db': 'test_db',
+    'db': 'cadvisor',
     'metric': 'cpu_usage_total',
     'epoch': 's',
     'filter': 'root_container_filter',
@@ -20,6 +21,20 @@ read_plugin = InfluxdbSeriesReadPlugin()
 read_plugin.init_app(app)
 
 reader = read_plugin.create(config)
-values = reader.read(secs_length=10000)
+values = reader.read(secs_length=100000)
 
 print(values)
+
+
+""" Test pandas series
+"""
+from core import seriesutils as su
+
+pdseries = su.timevaluepair_to_pdseries(values)
+# print(pdseries)
+
+pdminute = su.resample(pdseries, 4)
+series = su.get_newestseries(pdminute, 3)
+is_finish = len(series) != len(pdminute)
+print(series)
+print(is_finish)
