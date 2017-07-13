ad320ac91e5a6085fd7b1950a25721e0e1227837
dungvt.dev <vutrungdungbk@gmail.com>
2017-07-13 18:07:04 +0700

    add periodicity detect module

---
+++ app/modules/forcast/periodicity_detect/autoperiod.py
@@ -0,0 +1,110 @@
+import numpy as np
+# import matplotlib.pyplot as plt
+import pandas as pd
+from pandas import DataFrame
+from pandas.lib import Timestamp
+from scipy import signal
+from segmentation import segment, fit
+
+
+def period_detect(series, fs=1440, threshold=0.2, periodogram_candiate=8, max_error=0.005, segment_method="topdownsegment"):
+    # dau vao df theo dinh dang cua twitter
+    # fs: tan so lay mau (sample per day)
+    if not isinstance(series, pd.Series):
+        raise ValueError(("data must be a single data frame, "
+                          "list, or vector that holds numeric values."))
+
+    n = len(series)
+
+    # mien tan so
+    data_value_trans = series - series.median()
+    f, Pxx_den = signal.periodogram(
+        data_value_trans, fs, window=signal.get_window('hamming', n))
+    # chon nguong 40 %
+    threshold = threshold * np.max(Pxx_den)
+    index_period_candidate = [i for i in range(1, Pxx_den.size - 1) if (
+        (Pxx_den[i] > threshold) and (Pxx_den[i] > Pxx_den[i + 1]) and (Pxx_den[i] > Pxx_den[i - 1]))]
+    period_candidate = [f[i]
+                        for i in index_period_candidate if (f[i] < (n - 1) / fs)]
+    period_candidate_pxx = [Pxx_den[i]
+                            for i in index_period_candidate if (f[i] < (n - 1) / fs)]
+
+    t = {
+        'period': period_candidate,
+        'magnitude': period_candidate_pxx
+    }
+    period_candidate_point = pd.DataFrame(t)
+    # chi lay 1 so luong candidate nhat dinh
+    # ham nlargest tra lai thu tu theo manitude gian dan
+    # do do ung vien dau tien la ung vin co Pxx lon nhat
+    period_candidate_point = period_candidate_point.nlargest(
+        periodogram_candiate, 'magnitude')
+
+    lag = range(0, n - 1)
+    autocorr = [np.correlate(series, np.roll(series, -i))
+                [0] / series.size for i in lag]
+    # ACF_candidate = [autocorr[int(i*fs)] for i in period_candidate_point['period']]
+
+    final_all_period = []
+    for period_temp in period_candidate_point['period']:
+        startpoint = (int)(period_temp * fs)
+        temp = autocorr[startpoint]
+
+        begin_frame = np.max([(startpoint - fs), 0])
+        end_frame = np.min([startpoint + fs, len(autocorr)])
+
+        max = np.max(autocorr[begin_frame:end_frame])
+        min = np.min(autocorr[begin_frame:end_frame])
+        tb = (max + min) / 2
+        if(max - min > 0):
+            autocorr_normalize = (np.array(autocorr) - tb) / (max - min)
+        else:
+            return final_all_period
+        segments = []
+        try:
+            if(segment_method == "slidingwindowsegment"):
+                segments = segment.slidingwindowsegment(
+                    autocorr_normalize[begin_frame:end_frame], fit.regression, fit.sumsquared_error, max_error)
+            if (segment_method == "topdownsegment"):
+                segments = segment.topdownsegment(autocorr_normalize[begin_frame:end_frame], fit.regression,
+                                                  fit.sumsquared_error, max_error)
+            if (segment_method == "bottomupsegment"):
+                segments = segment.bottomupsegment(autocorr_normalize[begin_frame:end_frame], fit.regression,
+                                                   fit.sumsquared_error, max_error)
+
+        except:
+            pass
+
+        if len(segments) < 3:
+            continue
+        # check xem co la hill ko
+        # diem start point la 200 (trong khoang moi 401 diem dang xet)
+        # tim doan seg cua diem nay
+        # seg_index = 0
+        for i in range(0, len(segments)):
+            if startpoint - begin_frame < segments[i][2]:
+                seg_index = i
+                break
+        if ((seg_index < 2) or (seg_index > len(segments) - 2)):
+            continue
+        dh_trai = (segments[seg_index][3] - segments[seg_index][1]) - (
+            segments[seg_index - 1][3] - segments[seg_index - 1][1])
+        dh_phai = (segments[seg_index + 1][3] - segments[seg_index + 1][1]) - (
+            segments[seg_index][3] - segments[seg_index][1])
+
+        if ((dh_phai < 0) and (dh_trai < 0)):  # diem nam tren hill tien hanh tim closest peak
+            while (segments[seg_index][3] > segments[seg_index][1]):
+                # di tu trai sang phai
+                # khi nao ma dao ham con duong thi di tu trai sang phai
+                seg_index = seg_index + 1
+                if (seg_index > len(segments) - 2):
+                    break
+            while (segments[seg_index][3] < segments[seg_index][1]):
+                # khi nao dao ham con am thi di tu phai sang trai
+                seg_index = seg_index - 1
+                if ((seg_index < 2)):
+                    break
+            if ((seg_index >= 2) and (seg_index <= len(segments) - 2)):
+                final_period = segments[seg_index][2]
+                final_all_period.append(final_period + begin_frame)
+    return [x * 1.0 / fs for x in final_all_period]
---
+++ app/tests/manual_tests/modules/test_periodicity_detect.py
@@ -0,0 +1,13 @@
+import setup_test
+import pandas as pd
+import os
+from modules.forcast.periodicity_detect import period_detect
+
+base_path = os.path.dirname(__file__)
+path = os.path.join(base_path, '10min_workload.csv')
+
+data = pd.read_csv(path, header=None)
+data = data[0]
+
+pr = period_detect(data, threshold=0.1, fs=144)
+print(pr)
