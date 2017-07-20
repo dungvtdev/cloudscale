19f8951d6a06dff94985e04e57b5073f670b8c1b
dungvt.dev <vutrungdungbk@gmail.com>
2017-07-20 07:55:36 +0700

    update testbed script

---
+++ testbed/result/influxdb_simple_to_text_file.py
@@ -0,0 +1,53 @@
+import requests
+import pandas as pd
+import numpy as np
+import json
+
+endpoint = 'vm2:8086'
+db_name = 'cpu_usage_total'
+
+
+def get_data(begin, end):
+    url = 'http://{endpoint}/query'.format(endpoint=endpoint)
+
+    # time_filter = 'time > now() - {begin}s AND time < now() - {end}s'
+    time_filter = "time > '{begin}' AND time < '{end}'".format(begin=begin, end=end)
+    q = 'SELECT "value" FROM "cpu_usage_total" WHERE {time_filter} GROUP BY result fill(null)'.format(
+        time_filter=time_filter)
+
+    payload = {
+        'db': db_name,
+        'q': q,
+    }
+
+    r = requests.get(url, params=payload)
+    return r.text
+
+
+def convert_to_dataframe(values):
+    df = pd.DataFrame(values)
+    array = np.asarray(df[1])
+    time = pd.to_datetime(df[0], format='%Y-%m-%d %H:%M:%S')
+    convert = pd.DataFrame(array, index=time)
+    return convert
+
+
+if __name__ == '__main__':
+    begin = '2017-05-27 7:27:00'
+    end = '2017-05-27 11:04:00'
+
+    data = get_data(begin, end)
+    data = json.loads(data)
+
+    data = data['results'][0]['series']
+
+    real = next((it for it in data if not it['tags']['result']), None)
+    real = real['values']
+    real_convert = convert_to_dataframe(real)
+    real_convert.to_csv('real.data.csv', index=False, header=False)
+    # write file
+
+    predict = next((it for it in data if it['tags']['result'] == 'predict'), None)
+    predict = predict['values']
+    predict_convert = convert_to_dataframe(predict)
+    predict_convert.to_csv('predict.data.csv', index=False, header=False)
